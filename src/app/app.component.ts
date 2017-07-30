b314a5d5120b4ef61baac1827633eb229816d539
Bluelime Learning <bluelime247learning@gmail.com>
2017-07-30 16:05:34 +0100

    My first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,52 @@
 import { Component } from '@angular/core';
+import { DataService } from './data.service';
+import { trigger,state,style,transition,animate,keyframes } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: 
+  `<p [@myAnimation]='state' (click)="animatePara()">Show me the money!</p> 
+
+  `,  
+   
+  styles: [`
+
+  p {
+      width:400px;
+      background:yellow;
+      margin: 200px auto;
+      text-align:center;
+      padding:40px;
+      font-size: 2.5em;
+
+  }
+
+   `] ,
+  animations:	[
+
+    trigger('myAnimation',[
+      state('little', style({
+          transform: 'scale(1)',
+
+        })),
+      state('large', style({
+          transform: 'scale(1.2)',
+
+        })),
+      transition('little <=> large', animate('500ms ease-in')),
+
+      ]),
+
+  ]
+   
 })
 export class AppComponent {
-  title = 'app';
+
+  state: string = 'little';
+
+  animatePara() {
+
+    this.state = (this.state === 'little' ? 'large': 'little');
+
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MyTestComponentComponent } from './my-test-component/my-test-component.component';
+import { DataService } from './data.service';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyTestComponentComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
-  providers: [],
+  providers: [DataService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
